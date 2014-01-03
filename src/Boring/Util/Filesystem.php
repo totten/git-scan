2c23ea95645d8a12d9111922f9769a58b5d025a1
Tim Otten <totten@civicrm.org>
2014-01-02 23:59:33 -0800

    Filesystem - Add more helpers and test cases

--- src/Boring/Util/Filesystem.php
+++ src/Boring/Util/Filesystem.php
@@ -18,9 +18,56 @@ class Filesystem extends \Symfony\Component\Filesystem\Filesystem {
     }
     if (is_dir($res)) {
       return realpath($res);
-    } else {
+    }
+    else {
       return $res;
     }
   }
 
+  /**
+   * @param array $paths of string
+   * @return array updated paths
+   */
+  public function toAbsolutePaths($paths) {
+    $result = array();
+    foreach ($paths as $path) {
+      $result[] = $this->toAbsolutePath($path);
+    }
+    return $result;
+  }
+
+  public function isDescendent($child, $parent) {
+    $parent = rtrim($parent, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+    $child = rtrim($child, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+    if (strlen($parent) >= strlen($child)) {
+      return FALSE;
+    }
+    return ($parent == substr($child, 0, strlen($parent)));
+  }
+
+  public function formatPrettyPath($path, $basePaths) {
+    foreach ($basePaths as $basePath) {
+      if ($path == $basePath || $this->isDescendent($path, $basePath)) {
+        return rtrim($this->makePathRelative($path, dirname($basePath)), '/');
+      }
+    }
+    return $path;
+  }
+
+  /**
+   * @param string|array|Traversable $files
+   * @throws \RuntimeException
+   */
+  public function validateExists($files) {
+    if (!$files instanceof \Traversable) {
+      $files = new \ArrayObject(is_array($files) ? $files : array($files));
+    }
+
+    //foreach ($this->toIterator($files) as $file) {
+    foreach ($files as $file) {
+      if (!file_exists($file)) {
+        throw new \RuntimeException("File not found: $file");
+      }
+    }
+  }
 }
