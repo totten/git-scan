577cda5d561927633c304c218f095aa89fc2fc69
Tim Otten <totten@civicrm.org>
2014-01-02 23:58:32 -0800

    ProcessTest - Fix namespace

--- tests/Boring/Util/ProcessTest.php
+++ tests/Boring/Util/ProcessTest.php
@@ -1,8 +1,8 @@
 <?php
-namespace Boring;
+namespace Boring\Util;
 use Boring\Util\Process as ProcessUtil;
 
-class ProcessTest extends BoringTestCase {
+class ProcessTest extends \Boring\BoringTestCase {
   public function testRunOk_pass() {
     $process = ProcessUtil::runOk(new \Symfony\Component\Process\Process("echo times were good"));
     $this->assertEquals("times were good", trim($process->getOutput()));
@@ -10,7 +10,7 @@ class ProcessTest extends BoringTestCase {
 
   public function testRunOk_fail() {
     try {
-      $process = ProcessUtil::runOk(new \Symfony\Component\Process\Process("echo tragedy befell the software > /dev/stderr; exit 1"));
+      ProcessUtil::runOk(new \Symfony\Component\Process\Process("echo tragedy befell the software > /dev/stderr; exit 1"));
       $this->fail("Failed to generate expected exception");
     } catch (\Boring\Exception\ProcessErrorException $e) {
       $this->assertEquals("tragedy befell the software", trim($e->getProcess()->getErrorOutput()));
